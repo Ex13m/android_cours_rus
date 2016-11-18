dd39588b9f7aac16da31b8f521c6f9f215c2e2bd
Igor Novak <ex333m@gmail.com>
2016-11-18 19:56:27 +0100

    Старт проэкта

---
+++ app/src/main/java/cz/konsalting/www/android_1_lesson_3_home_work/MainActivity.java
@@ -0,0 +1,13 @@
+package cz.konsalting.www.android_1_lesson_3_home_work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/cz/konsalting/www/android_1_lesson_3_home_work/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package cz.konsalting.www.android_1_lesson_3_home_work;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
